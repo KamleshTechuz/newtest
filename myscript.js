28dde78dcd1cb3e625a232538383c36fc650e773
KamleshTechuz <kamlesh@techuz.com>
2022-01-21 12:33:45 +0530

    initial code

---
+++ myscript.js
@@ -0,0 +1,15 @@
+function ch_font(){
+    document.getElementById("demo").style.fontSize = '50px'
+}
+function ch_color(){
+    document.getElementById("demo").style.color = 'red'
+}
+function ch_text(){
+    document.getElementById("demo").innerHTML = "changed text"
+}
+function disp(){
+    document.getElementById("demo").style.display = "none"
+}
+function app(){
+    document.getElementById("demo").style.display = "block" 
+}
